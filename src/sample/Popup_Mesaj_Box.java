f29a77e06b0d5de1b1e10943065e8620186e6c7b
Obarey <ahmetkanbur@gmail.com>
2017-09-15 12:14:56 +0300

    guncelleme

--- src/sample/Popup_Mesaj_Box.java
+++ src/sample/Popup_Mesaj_Box.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.concurrent.Task;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -26,9 +27,10 @@ import java.util.ArrayList;
 public class Popup_Mesaj_Box extends ScrollPane {
 
     private String oto;
-    private JSONArray mesajlar;
+    private JSONArray gelen_mesajlar, giden_mesajlar;
     private Label notf_lbl;
     private VBox mesaj_ul;
+    private int tip = 1;
 
     public Popup_Mesaj_Box( String oto ){
         super();
@@ -46,12 +48,19 @@ public class Popup_Mesaj_Box extends ScrollPane {
         main.setSpacing(10);
         main.setAlignment(Pos.CENTER);
 
+
         HBox dp_container = new HBox();
         DatePicker dp = new DatePicker();
         dp.setValue(Common.dp_placeholder(Common.get_current_date()));
         dp.getStyleClass().add("grigrad");
         GButton getir_btn = new GButton("Getir", GButton.CMORK);
         notf_lbl = new Label("Veri bulunamadı!");
+
+        HBox tip_cont = new HBox();
+        tip_cont.setAlignment(Pos.CENTER);
+        tip_cont.setPadding(new Insets(5, 0, 5, 0));
+        GButton tip_btn = new GButton("Giden Mesajları Görüntüle", GButton.CMORK);
+        tip_cont.getChildren().add( tip_btn );
 
         dp_container.setAlignment(Pos.CENTER);
         dp_container.setSpacing(5);
@@ -61,11 +70,24 @@ public class Popup_Mesaj_Box extends ScrollPane {
         mesaj_ul.setAlignment(Pos.CENTER);
         mesaj_ul.setSpacing(10);
 
-        main.getChildren().addAll( dp_container, mesaj_ul );
+        main.getChildren().addAll( dp_container, tip_btn, mesaj_ul );
         setContent(main);
 
         update_data( "AKTIF");
         update_ui();
+
+        tip_btn.setOnMousePressed( ev -> {
+            tip_btn.setDisable(true);
+            if( tip == 0 ){
+                tip = 1;
+                tip_btn.setText("Giden Mesajları Görüntüle");
+            } else {
+                tip = 0;
+                tip_btn.setText("Gelen Mesajları Görüntüle");
+            }
+            update_ui();
+            tip_btn.setDisable(false);
+        });
 
         getir_btn.setOnMousePressed(ev -> {
             System.out.println(dp.getValue());
@@ -93,17 +115,29 @@ public class Popup_Mesaj_Box extends ScrollPane {
     }
 
     public void update_ui(){
-        if( mesajlar.length() == 0 ) {
-            notf_lbl.setText("Veri yok.");
+        JSONArray temp_data;
+        if( tip == 1 ){
+            // gelen
+            if( gelen_mesajlar.length() == 0 ) {
+                notf_lbl.setText("Veri yok.");
+            }
+            temp_data = gelen_mesajlar;
+        } else {
+            // giden
+            if( giden_mesajlar.length() == 0 ) {
+                notf_lbl.setText("Veri yok.");
+            }
+            temp_data = giden_mesajlar;
         }
+
         mesaj_ul.getChildren().clear();
         try {
             ImageView ico;
             Label mesaj, tarih;
             AnchorPane mesaj_li;
             JSONObject item;
-            for( int x = 0; x < mesajlar.length(); x++ ){
-               item = mesajlar.getJSONObject(x);
+            for( int x = 0; x < temp_data.length(); x++ ){
+               item = temp_data.getJSONObject(x);
                mesaj_li = new AnchorPane();
                mesaj_li.getStyleClass().add("li");
                ico = new ImageView( new Image(getClass().getResourceAsStream("resources/img/ico_obareybox_mesaj.png")));
@@ -129,8 +163,8 @@ public class Popup_Mesaj_Box extends ScrollPane {
             }
 
             notf_lbl.setText("");
-            System.out.println(mesajlar);
-            mesajlar = new JSONArray();
+            //System.out.println(temp_data);
+            //mesajlar = new JSONArray();
         } catch( Exception e ){
             e.printStackTrace();
         }
@@ -141,8 +175,9 @@ public class Popup_Mesaj_Box extends ScrollPane {
         Web_Request request = new Web_Request(Web_Request.SERVIS_URL, "&req=mesaj_download&oto="+oto+"&tarih="+tarih );
         request.kullanici_pc_parametreleri_ekle();
         request.action();
-        JSONObject data = new JSONObject(request.get_value()).getJSONObject("data");
-        mesajlar = data.getJSONArray("mesaj_data");
+        JSONObject data = new JSONObject(request.get_value()).getJSONObject("data").getJSONObject("mesaj_data");
+        gelen_mesajlar = data.getJSONArray("gelen");
+        giden_mesajlar = data.getJSONArray("giden");
 
     }
 
